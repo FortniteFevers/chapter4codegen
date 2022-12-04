c89b365c44c4e6a93a30eddc6fc614578cf380e5
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2022-12-03 20:43:19 -0500

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,16 +1,28 @@
 import requests
+import webbrowser
+import time
 import random
+
+codelength = 8
 
 list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
 data = 'https://www.fortnitechapter4.com/api/codes/'
 
 count = 0
+
+def g(rolls):
+	result = ""
+	while rolls >= 1:
+		c = random.choice(list)
+		result = c + result
+		rolls = rolls - 1
+	return result
+
 while True:
-    result = ''
-    for i in range(9):
-        pr = random.choice(list)
-        result += pr
+
+
+    result = f'{g(3)}-{g(2)}-{g(3)}'
 
     response = requests.get(f'https://www.fortnitechapter4.com/api/codes/{result}/data.json')
     if 'type' not in response:
@@ -18,4 +30,8 @@ while True:
         count += 1
     else:
         print(f'\nFOUND CODE!\n{result}')
+
+        url = f'https://www.fortnitechapter4.com/api/codes/{result}'
+        print(url)
+        time.sleep(100000)
         exit()
